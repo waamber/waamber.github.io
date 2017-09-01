0f26ab15ef343389842909e82e9b5df20702079f
Waamber <amberastuart@aol.com>
2017-09-01 14:28:39 -0500

    added click event

--- main.js
+++ main.js
@@ -3,11 +3,13 @@
 // blogcard.addEventListener("click", function(event){
 // 	console.log(event);
 // });
+var blogContainer = document.getElementById("blog-container");
+var selectedCard;
 
 function domString(blogs){
 	var blogString = "";
 	for(var i = 0; i < blogs.length; i ++){
-		blogString += `<section class="blog-card col-xs-4 panel panel-success" id="blogcard">
+		blogString += `<section class="blog-card col-xs-4 panel panel-success child" id="blogcard">
 											<div class='card-title panel-heading child'>${blogs[i].title}</div>
 											<div class='card-date panel-body child'><h6>${blogs[i].date}</h6></div>
 											<div class='card-content panel-body child'><p>${blogs[i].content}</p></div>
@@ -15,7 +17,20 @@ function domString(blogs){
 	writeToDom(blogString);
 	}
 };
+
+blogContainer.addEventListener("click", function(event){
+  if(event.target.classList.contains("child")){
+    selectedCard = event.target.parentNode;
+  }else if(event.target.parentNode.parentNode.classList.contains("blog-card")){
+    selectedCard = event.target.parentNode.parentNode;
+  }else if(event.target.classList.contains("blog-card")){
+    selectedCard = event.target;
+  }
+    return selectedCard;
+});
+
  
+
 function writeToDom(strang){
 	var blogContainer = document.getElementById("blog-container");
 	blogContainer.innerHTML = strang; 
@@ -30,10 +45,7 @@ function error() {
 	console.log("AHHHH!");
 };
 
-	var blogcard = document.getElementById("blog-card");
-		blogcard.addEventListener("click", function(event){
-	console.log(event);
-});
+
 
 var blogsRequest = new XMLHttpRequest;
 blogsRequest.addEventListener("load", loadBlogs);
