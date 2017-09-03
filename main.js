63cea4aaf4b9fa6d468b46ad1b747866e52290bf
Waamber <amberastuart@aol.com>
2017-09-03 14:18:01 -0500

    added jumbotron to selected card and hidden prop

--- main.js
+++ main.js
@@ -5,11 +5,12 @@
 // });
 var blogContainer = document.getElementById("blog-container");
 var selectedCard;
+var jumboCardContainer = document.getElementById("jumboCard");
 
 function domString(blogs){
 	var blogString = "";
 	for(var i = 0; i < blogs.length; i ++){
-		blogString += `<section class="blog-card child col-xs-4" id="blogcard">
+		blogString += `<section class="blog-card col-xs-4" id="blogcard">
 											<div class='card-title child'>${blogs[i].title}</div>
 											<div class='card-date child'><h6>${blogs[i].date}</h6></div>
 											<div class='card-content child'><p>${blogs[i].content}</p></div>
@@ -26,29 +27,14 @@ blogContainer.addEventListener("click", function(event){
   }else if(event.target.classList.contains("blog-card")){
     selectedCard = event.target;
   }
-    addModal();
-    console.log(selectedCard);
+  	jumboCardContainer.classList.remove("hidden");
+  	jumboCardContainer.innerHTML = selectedCard.innerHTML;
+    console.log(event);
 });
 
-//possible jumbotron with hidden class "display: none";
-
-function addModal(){
-	selectedCard.classList.remove("col-xs-4");
-	selectedCard.classList.add("modal-content", "stretch");
-	selectedCard.classList.add("col-xs-12");
-
-	var modalBtn = document.createElement("button");
-	selectedCard.appendChild(modalBtn);
-	var modalTxt = document.createTextNode("CLOSE");
-	modalBtn.appendChild(modalTxt);
-
-modalBtn.addEventListener("click", function(){
-			selectedCard.classList.remove("col-xs-12", "modal-content", "stretch");
-			selectedCard.classList.add("col-xs-4");
-	})
-}
-
-
+jumboCardContainer.addEventListener("click", function(){
+	jumboCardContainer.classList.add("hidden");
+})
 
 function writeToDom(strang){
 	var blogContainer = document.getElementById("blog-container");
@@ -64,15 +50,15 @@ function error() {
 	console.log("AHHHH!");
 };
 
-var inputField = document.getElementById("search");
+// var inputField = document.getElementById("search");
 
-inputField.addEventListener("keypress", function(event) {
-		var txt = inputField.value;
-		var result = blogs.filter(function(blogs) {
-			return blogs.content.indexOf(txt)>-1;
-		})
-		domString(result);
-});
+// inputField.addEventListener("keypress", function(event) {
+// 		var txt = inputField.value;
+// 		var result = blogs.filter(function(blogs) {
+// 			return blogs.content.indexOf(txt)>-1;
+// 		})
+// 		domString(result);
+// });
 
 var blogsRequest = new XMLHttpRequest;
 blogsRequest.addEventListener("load", loadBlogs);
